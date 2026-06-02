e06124bf6bfe73539a9b67f92ed41523eb41df43
phtea <phtea@mail.com>
2026-06-03 01:12:15 +0300

    Unneeded stuff to update ngl

--- init.el
+++ init.el
@@ -294,27 +294,6 @@
 
 
 ;;; ------------------------------------------------------------
-;;; Magit
-;;; ------------------------------------------------------------
-;; (use-package magit
-;;   :ensure magit
-;;   :config
-;;   (progn
-;;     (evil-set-initial-state 'magit-mode 'normal)
-;;     (evil-set-initial-state 'magit-status-mode 'normal)
-;;     (evil-set-initial-state 'magit-diff-mode 'normal)
-;;     (evil-set-initial-state 'magit-log-mode 'normal)
-;;     (evil-define-key 'normal magit-mode-map
-;;       "j" 'evil-next-visual-line
-;;       "k" 'evil-previous-visual-line)
-;;     (evil-define-key 'normal magit-log-mode-map
-;;       "j" 'evil-next-visual-line
-;;       "k" 'evil-previous-visual-line)
-;;     (evil-define-key 'normal magit-diff-mode-map
-;;       "j" 'evil-next-visual-line
-;;       "k" 'evil-previous-visual-line)))
-
-;;; ------------------------------------------------------------
 ;;; Helpful functions
 ;;; ------------------------------------------------------------
 
